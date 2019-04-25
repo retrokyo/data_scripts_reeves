6eb9f0c04ec172adfc1ca3950dad99ad67f68388
Reeves <froningr@gmail.com>
2019-04-25 18:14:50 +0900

    Added functions for item/ time totals of path

--- pathing/node_calculation.py
+++ pathing/node_calculation.py
@@ -1,8 +1,13 @@
 import folium as fm
+from random import randrange
 
 from node_class import *
 
+'''
+Collection of functions to calculate the node pathing
 
+TO-DO: Figure out how I want to remove nodes that have been pathed from the node_list
+'''
 def node_map(node_list, city='tokyo', save=False):
     '''
     Create a map from a collection of node, and drop_node objects.\n
@@ -25,10 +30,8 @@ def node_map(node_list, city='tokyo', save=False):
                         tooltip=fm.Tooltip(text=node_list[0].retrieve_node_info()),
                         icon=fm.Icon(color='green', icon='angle-up', prefix='fa')).add_to(node_map)
     
-    travel_path = [node_list[0]]
-    node_list.pop(0)
     
-    for node in node_list:
+    for node in node_list[1:]:
         
         node.calculate_node_info(travel_path[0])
 
@@ -43,17 +46,95 @@ def node_map(node_list, city='tokyo', save=False):
 
     return node_map
 
-def node_path_calc(node_list):
-    node_path = []
+def node_path_calc(node_list, item_limit=100, time_limit=60):
+    '''
+    Calculates a node path based on first best option\n
 
-    for node in node_list:
-        node.calculate_node_info[node_list[0]]
+    Parameters:\n
+    node_list: List containing node type objects. First object in list should be of object node
 
-    node_path.append(node_list[0])
-    node_list.pop[0]
+    Return:
+
+    '''
+    node_list_copy = node_list[:]
+    node_path = [node_list_copy[0]]
+
+    while (node_path_item_sum(node_path) < item_limit & node_path_time_sum(node_path) < time_limit & node_list_copy):
+        node_path.append(next_best_node(node_list_copy))
+        node_list_copy.pop(0)
+
+    return node_path
+
+def next_best_node(node_list):
+    for node in node_list[1:]:
+        node.calculate_node_info(node_list[0])
+
+    sort_nodes(node_list, 1)
+
+    return node_list[1]
+
+
+def sort_nodes(node_list, start=0, end=len(node_list)-1):
+    '''
+    Sort node objects using quicksort method, inplace\n
+
+    Parameters:\n
+    node_list: list containing node objects\n
+    start: start index\n
+    end: end index\n
+
+    Return\n
+    List of node objects sorted on the '__distance_to' property
+    '''
+    if start >= end:
+        return
     
-    for node in node_list:
-        pass
+    pivot_idx = randrange(start, end, 1)
+    pivot_node = node_list[pivot_idx]._get_distance_to
 
-def sort_nodes(node_list):
-    pass
+    node_list[end], node_list[pivot_idx] = node_list[pivot_idx], node_list[end]
+
+    lt_pointer = start
+
+    for i in range(start, end):
+
+        if node_list[i]._get_distance_to < pivot_node:
+            node_list[i], node_list[lt_pointer] = node_list[lt_pointer], node_list[i]
+            lt_pointer += 1
+
+    node_list[end], node_list[lt_pointer] = node_list[lt_pointer], node_list[end]
+
+    sort_nodes(node_list, start, lt_pointer-1)
+    sort_nodes(node_list, lt_pointer+1, end)
+
+def node_path_item_sum(node_path):
+    '''
+    Calculates the node path's item count sum\n
+
+    Parameters:\n
+    node_path: list of node object\n
+
+    Return:\n
+    The sum of the nodes' item count in the current path
+    '''
+    item_count_sum = 0
+    for node in node_path:
+        item_count_sum += node.item_count
+
+    return item_count_sum
+
+def node_path_time_sum(node_path):
+    '''
+    Calculates the node path's time sum\n
+
+    Parameters:\n
+    node_path: list of node objects\n
+
+    Return:\n
+        The sum of the nodes' time in the current path
+    '''
+    time_sum = 0.0
+    for node in node_path:
+        time_sum += node._get_time_to
+
+    return time_sum
